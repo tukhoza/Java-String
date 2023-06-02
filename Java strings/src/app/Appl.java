d784252fc5a193c9dbe13bc4326c750bdf6ced62
tukhoza <39373673+tukhoza@users.noreply.github.com>
2023-06-02 22:52:06 +0100

    String project

---
+++ Java strings/src/app/Appl.java
@@ -0,0 +1,25 @@
+package app;
+
+public class Appl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greetingText = "Hello word";
+		
+		System.out.println(greetingText);
+		
+		String perposeText = "\nThis is my first string project.";
+		String secondPerposeText = "\nWhere I join two strings together";
+		String thirdPerposeText = "using new line  and joining primative with string like \nnumber : ";
+		
+		String firstSentence = perposeText + secondPerposeText;
+		int numberSeven = 7;
+		
+		System.out.println(firstSentence);
+		System.out.println(thirdPerposeText+numberSeven);
+		
+		
+		
+	}
+
+}
